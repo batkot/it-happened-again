5b5ea059d3355e3f42bd2b677d9e766ebc2563c9
Tomasz Batko <batkot@protonmail.com>
2019-07-04 22:40:27 +0200

    Playing around with Application Commands

--- src/ItHappenedAgain.hs
+++ src/ItHappenedAgain.hs
@@ -1,30 +1,94 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module ItHappenedAgain where
+module ItHappenedAgain 
+    ( CreateTracking
+    , createTracking
+    , createTrackingHandler
 
-import DomainDrivenDesign.MTL
+    , TrackOccurance
+    , trackOccurance
 
+    , FinishTracking
+    , finishTracking
+
+    , CommandHandler(..)
+    ) where
+
+import DomainDrivenDesign.Aggregate
 import ItHappenedAgain.Tracker
 
 import Data.Text (Text)
+import Data.Time (UTCTime)
 
 data CreateTracking = CreateTracking
     { ctId :: !Int 
     , ctName :: !Text
     }
 
--- | This thing should build some kind of event processor
-createTracking 
-    :: forall m. Monad m 
-    => (Int -> m [Event]) 
-    -> CreateTracking 
-    -> m ()
-createTracking fetchEvents CreateTracking{..} = 
-    fetchEvents ctId >>= runAggregateActionT action >>= storeEvents 
-  where
-    storeEvents = const $ return ()
-    trackId = TrackingId ctId
-    action :: AggregateActionT Tracking Event Error m ()
-    action = create trackId ctName 
+data CommandHandler key m = CommandHandler
+    { aggregateId :: key
+    , action :: m ()
+    }
 
+createTrackingHandler 
+    :: MonadAggregateAction Tracking Event Error m
+    => CreateTracking
+    -> CommandHandler Int m
+createTrackingHandler CreateTracking{..} =
+    CommandHandler ctId $ create trackId ctName 
+  where
+    trackId = TrackingId ctId
+
+createTracking
+    :: MonadAggregateAction Tracking Event Error m
+    => CreateTracking
+    -> m ()
+createTracking CreateTracking{..} =
+    create trackId ctName
+  where
+    trackId = TrackingId ctId
+
+data TrackOccurance = TrackOccurance
+    { tId :: !Int
+    , tTime :: UTCTime 
+    , tGeoCords :: Maybe (Int, Int)
+    }
+
+trackOccurance
+    :: MonadAggregateAction Tracking Event Error m
+    => (Int -> m ()) -- repository ??
+    -> TrackOccurance
+    -> m ()
+trackOccurance fetchAggregate TrackOccurance{..} =
+    fetchAggregate tId >> track tTime geoCords
+  where
+    geoCords = fmap GeoCords tGeoCords
+
+data FinishTracking = FinishTracking
+    { fId :: !Int
+    , finishTime :: !UTCTime
+    }
+
+finishTracking 
+    :: MonadAggregateAction Tracking Event Error m
+    => (Int -> m ()) -- repository ??
+    -> FinishTracking
+    -> m ()
+finishTracking fetchAggregate FinishTracking{..} =
+    fetchAggregate fId >> finish finishTime
+
+-- | This thing should build some kind of event processor
+-- createTracking 
+--    :: forall m. Monad m 
+--    => (Int -> m [Event]) 
+--    -> CreateTracking 
+--    -> m ()
+--createTracking fetchEvents CreateTracking{..} = 
+--    fetchEvents ctId >>= runAggregateActionT action >>= storeEvents 
+--  where
+--    storeEvents = const $ return ()
+--    trackId = TrackingId ctId
+--    action :: AggregateActionT Tracking Event Error m ()
+--    action = create trackId ctName 
